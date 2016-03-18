bebf3c332be3a34d2003ab0f27347db166265ae7
sokasyn <sokasyn@qq.com>
2016-03-18 16:00:41 +0800

    Project created

---
+++ BackgroundTaskTest/BackgroundTaskTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackgroundTaskTest
+//
+//  Created by 李星月 on 16/3/18.
+//  Copyright © 2016年 com.emin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackgroundTaskTest/BackgroundTaskTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackgroundTaskTest
+//
+//  Created by 李星月 on 16/3/18.
+//  Copyright © 2016年 com.emin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
